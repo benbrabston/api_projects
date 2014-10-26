aa6d7fc7f05c914ff96a8dc36bc74f938c65350c
benbrabston <BenBrabston@Bens-MacBook-Pro-3.local>
2014-10-26 17:20:41 -0500

    Homework_Complete

--- address_to_weather.rb
+++ address_to_weather.rb
@@ -7,10 +7,38 @@ puts "What is the address you would like to know the weather for?"
 the_address = gets.chomp
 url_safe_address = URI.encode(the_address)
 
-# Your code goes below.
+url_location = "http://maps.googleapis.com/maps/api/geocode/json?address=" + url_safe_address
 
-# Ultimately, we want the following line to work when uncommented:
+raw_data = open(url_location).read
+parsed_data = JSON.parse(raw_data)
 
-# puts "The current temperature at #{the_address} is #{the_temperature} degrees."
-# puts "The outlook for the next hour is: #{the_hour_outlook}"
-# puts "The outlook for the next day is: #{the_day_outlook}"
+results = parsed_data["results"]
+first = results[0]
+geometry = first["geometry"]
+location = geometry["location"]
+
+
+the_latitude = location["lat"]
+the_longitude = location["lng"]
+
+url_weather = "https://api.forecast.io/forecast/f33d281c4d30d6dcd6850cd3bf94f2d2/" + the_latitude.to_s + "," + the_longitude.to_s
+
+raw_data = open(url_weather).read
+parsed_data = JSON.parse(raw_data)
+currently = parsed_data["currently"]
+
+# Current temperature
+the_temperature = currently["temperature"]
+
+# Hour outlook
+hourly = parsed_data["hourly"]
+the_hour_outlook = hourly["summary"]
+
+# Next day outlook
+daily = parsed_data["daily"]
+the_day_outlook = daily["summary"]
+
+
+puts "The current temperature at #{the_address} is #{the_temperature} degrees."
+puts "The outlook for the next hour is: #{the_hour_outlook}"
+puts "The outlook for the next day is: #{the_day_outlook}"
--- coords_to_weather.rb
+++ coords_to_weather.rb
@@ -9,12 +9,25 @@ the_latitude = gets.chomp
 puts "What is the longitude?"
 the_longitude = gets.chomp
 
-# Your code goes below. Use the same approach as you did in
-#   address_to_coords.rb to read from a remote API and parse
-#   the results.
+url = "https://api.forecast.io/forecast/f33d281c4d30d6dcd6850cd3bf94f2d2/" + the_latitude + "," + the_longitude
+
+raw_data = open(url).read
+parsed_data = JSON.parse(raw_data)
+currently = parsed_data["currently"]
+
+# Current temperature
+the_temperature = currently["temperature"]
+
+# Hour outlook
+hourly = parsed_data["hourly"]
+the_hour_outlook = hourly["summary"]
+
+# Next day outlook
+daily = parsed_data["daily"]
+the_day_outlook = daily["summary"]
 
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The current temperature at #{the_latitude}, #{the_longitude} is #{the_temperature} degrees."
-# puts "The outlook for the next hour is: #{the_hour_outlook}"
-# puts "The outlook for the next day is: #{the_day_outlook}"
+puts "The current temperature at #{the_latitude}, #{the_longitude} is #{the_temperature} degrees."
+puts "The outlook for the next hour is: #{the_hour_outlook}"
+puts "The outlook for the next day is: #{the_day_outlook}"
